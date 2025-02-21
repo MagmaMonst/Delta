2864e81dce03a85555d8c524ce9498924ba16d1b
Udbhav V <udbhavv@Udbhavs-MacBook-Air.local>
2025-02-21 23:17:22 +0530

    updated backend to run on localhost and linked frontend

--- chat-app-server/index.js
+++ chat-app-server/index.js
@@ -1,15 +1,21 @@
 import express from "express";
+import cors from "cors";
 import {Server as socketIO_Server} from "socket.io";
 import {createServer as HTTP_createServer} from "node:http";
+const corsOptions = {
+    origin : ["http://localhost:5173"]
+};
 
 const app = express();
 const server = HTTP_createServer(app);
 const io = new socketIO_Server(server);
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+    res.send("Annoying chat app!");
 })
 
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log("server running at http://localhost:3000");
 });
