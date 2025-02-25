e9c75a537e75e4f7e62e86db476632f3c132fac8
Serendipitous19 <aahan1729@gmail.com>
2025-02-25 17:57:45 +0530

    wrote function to validate users given username and password, and tested the createuser and authenticateuser POST routes (there were so many bugs lol)

--- chat-app-server/User.js
+++ chat-app-server/User.js
@@ -1,5 +1,16 @@
 import bcrypt from "bcrypt";
-import { addDoc, getDoc, collection } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+
+export function getUserDocRef(db, username) {
+    return doc(db, "users", username);
+}
+
+export async function getUser(userDocRef) {
+    const userDocSnapshot = await getDoc(userDocRef);
+    if (!userDocSnapshot.exists()) return false;
+    return userDocSnapshot.data();
+}
+
 export async function addUser(db, username, plaintextPassword) {
     const user = {
         username: username,
@@ -7,15 +18,26 @@ export async function addUser(db, username, plaintextPassword) {
         gcs: [],
         password: await bcrypt.hash(plaintextPassword, 11)
     };
-    try {
-        const userDocRef = await addDoc(collection(db, "users"), user);
-        const userDocSnapshot = getDoc(userDocRef);
-        return userDocSnapshot;
-    } catch (e) {
-        console.error(e);
-    }
+
+    const userDocRef = getUserDocRef(db, username);
+
+    const userDoc = await getUser(userDocRef);
+    if (userDoc !== false) return { added: false, msg: "Username already exists, sorry" };
+
+    await setDoc(userDocRef, user);
+    return { added: true, msg: "User added successfully!" };
 }
 
 export async function validateUser(db, username, plaintextPassword) {
+    const userDocRef = getUserDocRef(db, username);
 
+    const userDoc = await getUser(userDocRef);
+    if (!userDoc) return { valid: false, msg: "No user with given username exists" };
+
+    const result = await bcrypt.compare(plaintextPassword, userDoc.password);
+    if (result) {
+        return { valid: true, msg: "Username and password are valid!" };
+    } else {
+        return { valid: false, msg: "Invalid password" };
+    }
 }
--- chat-app-server/index.js
+++ chat-app-server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import { Server as socketIO_Server } from "socket.io";
 import { createServer as HTTP_createServer } from "node:http";
@@ -7,7 +8,6 @@ import { initializeApp as f_initializeApp } from "firebase/app";
 import { getFirestore as f_getFirestore } from "firebase/firestore";
 import { addUser, validateUser } from "./User.js";
 import jwt from "jsonwebtoken";
-import {generateToken} from "./auth";
 
 dotenv.config();
 const firebaseConfig = {
@@ -23,15 +23,13 @@ const firebaseApp = f_initializeApp(firebaseConfig);
 
 const db = f_getFirestore(firebaseApp);
 
-
-
-addUser(db, "user-8", "weakpassword").then((userDocRef) => {
-	console.log(userDocRef.data());
-});
-
 const app = express();
 const server = HTTP_createServer(app);
 const io = new socketIO_Server(server);
+
+// for processing bodies in http requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
 	origin: ["http://localhost:5173"]
@@ -40,24 +38,37 @@ app.use(cors(corsOptions));
 
 app.post("/createuser", async (req, res) => {
 	// add username and hashed password to database
-	await addUser(db, req.body.username, req.body.password);
+	if (req.body && req.body.username && req.body.password) {
+		let { added, msg } = await addUser(db, req.body.username, req.body.password);
+		if(added) {
+			res.status(200).send({added: true, msg: msg});
+		} else {
+			res.status(400).send({added: false, msg: msg});
+		}
+	} else {
+		res.status(400).json({ added: false, msg: "Invalid request, please provide username and password fields in request body" });
+	}
 });
 
 app.post("/authenticateuser", async (req, res) => {
-	// check if user has provided correct credentials from database
-	const username = req.body.username;
-	const password = req.body.password;
-	const isValid = await validateUser(db, username, password);
-	// if yes, provide an authentication token thingy (JWT)
-	if (isValid) {
-		const data = {
-			time: Date.now(),
-			username: username,
-		};
-		const token = jwt.sign(data, process.env.JWT_SECRET_KEY);
-		res.send(token);
+	if (req.body && req.body.username && req.body.password){
+		// check if user has provided correct credentials from database
+		const username = req.body.username;
+		const password = req.body.password;
+		const { valid, msg } = await validateUser(db, username, password);
+		// if yes, provide an authentication token thingy (JWT)
+		if (valid) {
+			const data = {
+				time: Date.now(),
+				username: username,
+			};
+			const token = jwt.sign(data, process.env.JWT_SECRET_KEY);
+			res.status(200).json({ token: token, msg: "here's your token :)" });
+		} else {
+			res.status(401).json({ token: null, msg: msg });
+		}
 	} else {
-		res.status(401).json("Error: invalid username or password");
+		res.status(400).json({ added: false, msg: "Invalid request, please provide username and password fields in request body" });
 	}
 });
 
