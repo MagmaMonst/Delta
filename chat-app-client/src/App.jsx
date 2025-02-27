f8e4934fc4622a62ddce14916bc80878d8c80991
Udbhav V <udbhavv@Udbhavs-MacBook-Air.local>
2025-02-27 22:48:02 +0530

    added login component for react

--- chat-app-client/src/App.jsx
+++ chat-app-client/src/App.jsx
@@ -1,53 +1,42 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import axios from 'axios';
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
-function Message({ count }) {
-	const [message, setMessage] = useState(null);
+const Login = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
+	
+	const manageLogin = async () => {
 
-	async function fetchAPI() {
-		const response = await axios.get(`http://localhost:3000/api?count=${count}`);
-		return response;
-	};
+	try {
+		const response = await axios.post('http://localhost:3000/authenticateuser', {username: '', password: ''});
+		const { token } = response.data;
+		localStorage.setItem('jwtToken', token);
 
-	useEffect(() => {
-		fetchAPI().then(result => {
-			setMessage(result.data);
-		});
-	}, [count]);
-
-	if(message === null) {
-		return <p>No messages from server!</p>;
-	}else {
-		return <p>Message from server: {message}</p>;
+	} catch (error) {
+		console.error('Login failed: ', error.response?.data?.message || error.message);
 	}
+};
 }
+
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<Message count={count}></Message>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<div class="chat-box">
+      			<div class="message-display">
+        		<div class="message user" >test for messages sent by user</div>
+        		<div class="message">test for messages sent by others</div>
+      		</div>
+      		<div class="input-background">
+        		<input type="text" class="message-input" placeholder="Type a message here"></input>
+        		<button class="send">Send</button>
+      		</div>
+    		</div>
 		</>
 	)
 }
