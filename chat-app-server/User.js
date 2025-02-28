1d11b05382ac0fa4ba8115178b9e0d857da9a7f7
Serendipitous19 <aahan1729@gmail.com>
2025-02-28 19:57:53 +0530

    chat rooms on the backend

--- chat-app-server/User.js
+++ chat-app-server/User.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, collection, query, where, setDoc, getDoc, addDoc } from "firebase/firestore";
 
 export function getUserDocRef(db, username) {
     return doc(db, "users", username);
@@ -14,9 +14,9 @@ export async function getUser(userDocRef) {
 export async function addUser(db, username, plaintextPassword) {
     const user = {
         username: username,
-        dms: [],
-        gcs: [],
-        password: await bcrypt.hash(plaintextPassword, 11)
+        password: await bcrypt.hash(plaintextPassword, 11),
+        rooms: [], // refs to each room,
+        roomJoinRequests: []
     };
 
     const userDocRef = getUserDocRef(db, username);
@@ -40,4 +40,85 @@ export async function validateUser(db, username, plaintextPassword) {
     } else {
         return { valid: false, msg: "Invalid password" };
     }
+}
+
+export async function createRoom(db, roomName) {
+    const roomsCollectionRef = collection(db, "rooms");
+    const roomRef = await addDoc(roomsCollectionRef, {
+        name: roomName,
+        users: [],
+        tentativeUsers: [], // users who haven't accepted the request to be added yet
+        messages: []
+    });
+    return roomRef;
+}
+
+export async function addUserToRoom(db, username, roomRef) {
+    const userRef = getUserDocRef(db, username);
+    const user = await getUser(userRef);
+    if (user === false) {
+        return { added: false, msg: "User does not exist" };
+    }
+    const roomSnapshot = await getDoc(roomRef);
+    if (!roomSnapshot.exists()) {
+        return { added: false, msg: "Room does not exist" };
+    }
+
+    user.roomJoinRequests.push(getRoomID(roomRef));
+    await setDoc(userRef, user);
+
+    const roomData = roomSnapshot.data();
+    roomData.tentativeUsers.push(username);
+    await setDoc(roomRef, roomData);
+    return { added: true, msg: `User ${username} added successfully to room ${roomData.name}` };
+}
+
+export async function acceptRoomAddRequest(db, username, roomRef) {
+    const userRef = getUserDocRef(db, username);
+    const user = await getUser(userRef);
+    if (user === false) {
+        return { success: false, msg: "User does not exist" };
+    }
+
+    const roomSnapshot = await getDoc(roomRef);
+    if (!roomSnapshot.exists()) {
+        return { success: false, msg: "Room does not exist" };
+    }
+    const roomData = roomSnapshot.data();
+
+    const indexOfUser = roomData.tentativeUsers.indexOf(username);
+    if (indexOfUser === -1) {
+        return { success: false, msg: "User has not been asked to join group, or already has joined group" };
+    }
+
+    const indexOfRequest = user.roomJoinRequests.indexOf(getRoomID(roomRef));
+    if (indexOfRequest === -1) {
+        console.log(user.roomJoinRequests);
+        return { success: false, msg: "User has not been added to group" };
+    }
+
+    roomData.tentativeUsers.splice(indexOfUser, 1);
+    roomData.users.push(username);
+    await setDoc(roomRef, roomData);
+
+    user.roomJoinRequests.splice(indexOfRequest, 1);
+    user.rooms.push(getRoomID(roomRef));
+    await setDoc(userRef, user);
+
+    return { success: true, msg: `User ${username} successfully joined room ${roomData.name}` };
+}
+
+export function getRoomRef(db, roomID) {
+    return doc(db, "rooms", roomID);
+}
+
+export function getRoomID(roomRef) {
+    return roomRef.id;
+}
+
+export async function getRoomData(db, roomID) {
+    const roomRef = getRoomRef(db, roomID);
+    const roomSnapshot = await getDoc(roomRef);
+    const roomData = roomSnapshot.data();
+    return roomData;
 }
--- chat-app-server/index.js
+++ chat-app-server/index.js
@@ -6,7 +6,7 @@ import { createServer as HTTP_createServer } from "node:http";
 import dotenv from "dotenv";
 import { initializeApp as f_initializeApp } from "firebase/app";
 import { getFirestore as f_getFirestore } from "firebase/firestore";
-import { addUser, validateUser, getUser, getUserDocRef } from "./User.js";
+import { addUser, validateUser, getUserDocRef, getUser, createRoom, addUserToRoom, acceptRoomAddRequest, getRoomRef, getRoomID } from "./User.js";
 import jwt from "jsonwebtoken";
 
 dotenv.config();
@@ -36,14 +36,72 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+const tokenMaxLifespan = 1000 * 60 * 60 * 24 * 30; // 30 days
+async function validateToken(token) {
+	// validate the token
+	try {
+		const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
+		if (verified.username && verified.time) {
+			const tokenLife = Date.now() - verified.time;
+			if (tokenLife < 0) {
+				return {
+					status: 401,
+					json: { authorized: false, msg: "It appears you are a time traveller. The server sends its deepest apologies, but it lacks the ability to cater to your needs. Sorry!" }
+				};
+			} else if (tokenLife > tokenMaxLifespan) {
+				return {
+					status: 401,
+					json: { authorized: false, msg: "Token expired, sorry. Sign in again to get a new one" }
+				};
+
+			} else {
+				return {
+					status: 200,
+					json: { authorized: true, msg: "Authorized!" },
+					token: verified
+				};
+			}
+		} else {
+			return {
+				status: 401,
+				json: { authorized: false, msg: "Invalid token sent. Try signing in again." }
+			};
+		}
+	} catch (e) {
+		return {
+			status: 401,
+			json: { authorized: false, msg: "ERROR: " + e }
+		};
+	}
+}
+
+let onlineUserSockets = {}; // for each user online, their socket
+function isUserOnline(username) {
+	return onlineUserSockets[username] !== undefined;
+}
+function notifyUserToUpdate(username) {
+	if (isUserOnline(username)) {
+		onlineUserSockets[username].emit("update");
+	}
+}
+
+function isAlphanumericWithUnderscore(str) {
+	const regex = /^[a-zA-Z0-9_]+$/;
+	return regex.test(str);
+}
+
 app.post("/createuser", async (req, res) => {
 	// add username and hashed password to database
 	if (req.body && req.body.username && req.body.password) {
-		let { added, msg } = await addUser(db, req.body.username, req.body.password);
-		if (added) {
-			res.status(200).send({ added: true, msg: msg });
+		if (isAlphanumericWithUnderscore(req.body.username)) {
+			let { added, msg } = await addUser(db, req.body.username, req.body.password);
+			if (added) {
+				res.status(200).send({ added: true, msg: msg });
+			} else {
+				res.status(400).send({ added: false, msg: msg });
+			}
 		} else {
-			res.status(400).send({ added: false, msg: msg });
+			res.status(400).json({ added: false, msg: "Username may only contain alphabets, numerals or underscores." });
 		}
 	} else {
 		res.status(400).json({ added: false, msg: "Invalid request, please provide username and password fields in request body" });
@@ -55,7 +113,6 @@ app.post("/authenticateuser", async (req, res) => {
 		// check if user has provided correct credentials from database
 		const username = req.body.username;
 		const password = req.body.password;
-		console.log(username);
 		const { valid, msg } = await validateUser(db, username, password);
 		// if yes, provide an authentication token thingy (JWT)
 		if (valid) {
@@ -73,30 +130,121 @@ app.post("/authenticateuser", async (req, res) => {
 	}
 });
 
-const tokenMaxLifespan = 1000 * 60 * 60 * 24 * 30; // 30 days
+app.post("/createroom", async (req, res) => {
+	if (req.headers && req.headers.jwt && req.body && req.body.roomName) {
+		const token = req.headers.jwt;
+		const roomName = req.body.roomName;
 
-app.get("/secureendpoint", async (req, res) => {
-	// validate the token
-	const token = req.headers.jwt;
-	try {
-		const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-		if(verified.username && verified.time) {
-			const tokenLife = Date.now() - verified.time;
-			if(tokenLife < 0) {
-				res.status(401).json({authorized: false, msg: "It appears you are a time traveller. The server sends its deepest apologies, but it lacks the ability to cater to your needs. Sorry!"});
-			} else if(tokenLife > tokenMaxLifespan) {
-				res.status(401).json({authorized: false, msg: "Token expired, sorry. Sign in again to get a new one"});
+		const validationResult = await validateToken(token);
+		if (validationResult.status === 200) {
+			const parsedToken = validationResult.token;
+			const username = parsedToken.username;
+
+			const newRoom = await createRoom(db, roomName);
+			const addStatus = await addUserToRoom(db, username, newRoom);
+			if (!addStatus.added) {
+				res.status(400).json({ created: false, msg: addStatus.msg });
 			} else {
-				const userID = verified.userID;
-				const user = await getUser(getUserDocRef(db, verified.username));
-				res.status(200).json({authorized: true, msg: `Congrats! You, ${user.username}, are authorized to recieve data...if only we had any data for you. But we do know your password hash is ${user.password}`});
+				const acceptStatus = await acceptRoomAddRequest(db, username, newRoom);
+				if (!acceptStatus.success) {
+					res.status(400).json({ created: false, msg: acceptStatus.msg });
+				} else {
+					res.status(200).json({ created: true, msg: "Successfully created room!" });
+				}
 			}
 		} else {
-			res.status(401).json({authorized: false, msg: "Invalid token sent. Try signing in again."});
+			res.status(validationResult.status).json(validationResult.json);
 		}
-	} catch (e) {
-		res.status(401).json({authorized: false, msg: "ERROR: " + e}); // token is wrong?
+	} else {
+		res.status(400).json({ created: false, msg: "Invalid request sent" });
 	}
+});
+
+app.post("/adduser", async (req, res) => {
+	if (req.headers && req.headers.jwt && req.body && req.body.roomID && req.body.userToAdd) {
+		// req.body.users is a comma-separated list of users to add
+		const token = req.headers.jwt;
+		const userToAdd = req.body.userToAdd;
+		const roomID = req.body.roomID;
+
+		const validationResult = await validateToken(token);
+		if (validationResult.status === 200) {
+			const parsedToken = validationResult.token;
+			const username = parsedToken.username;
+			const userRef = getUserDocRef(db, username);
+			const user = await getUser(userRef);
+			const roomRef = getRoomRef(db, roomID);
+
+			if (user.rooms && user.rooms.includes(roomID)) { // no, it includes only ids
+				const addStatus = await addUserToRoom(db, userToAdd, roomRef); // todo: fix bug, ths shd be a ref to room, not name
+				if (!addStatus.added) {
+					res.status(400).json({ requested: false, msg: addStatus.msg });
+				} else {
+					notifyUserToUpdate(userToAdd);
+					res.status(200).json({ requested: true, msg: "successfully requested user to join room" });
+				}
+			} else {
+				res.status(400).json({ requested: false, msg: "You cannot request people to join a room you are not in" });
+			}
+		} else {
+			res.status(validationResult.status).json(validationResult.json);
+		}
+	} else {
+		res.status(400).json({ requested: false, msg: "Invalid request sent" });
+	}
+});
+
+app.post("/acceptjoinrequest", async (req, res) => {
+	if (req.headers && req.headers.jwt && req.body && req.body.roomID) {
+		// req.body.users is a comma-separated list of users to add
+		const token = req.headers.jwt;
+		const roomID = req.body.roomID;
+
+		const validationResult = await validateToken(token);
+		if (validationResult.status === 200) {
+			const parsedToken = validationResult.token;
+			const username = parsedToken.username;
+			const roomRef = getRoomRef(db, roomID);
+			const acceptStatus = await acceptRoomAddRequest(db, username, roomRef);
+			if(!acceptStatus.success) {
+				res.status(400).send({ accepted: false, msg: acceptStatus.msg});
+			} else {
+				notifyUserToUpdate(username);
+				res.status(200).send({accepted: true, msg: acceptStatus.msg});
+			}
+		} else {
+			res.status(validationResult.status).json(validationResult.json);
+		}
+	} else {
+		res.status(400).json({ accepted: false, msg: "Invalid request sent" });
+	}
+});
+
+io.on("connection", (socket) => {
+	console.log(`socket ${socket.id} connected`);
+	let userData = {
+		userVerified: false
+	};
+	io.on("validate-token", async (jwt, callback) => {
+		const validationResult = await validateToken(jwt);
+		if (validationResult.status === 200) {
+
+			userData.userVerified = false;
+			userData.username = validationResult.token.username;
+
+
+			callback({
+				valid: true,
+				msg: validationResult.msg,
+				userData: userData
+			});
+		} else {
+			callback({
+				valid: false,
+				msg: validationResult.msg
+			});
+		}
+	});
 });
 
 app.listen(3000, () => {
